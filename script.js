b1841e09a1d4f1fceef1238a6d444b801c5b90c2
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-10-09 15:06:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ navbar.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+//Saiba mais
+const boxH2 = document.querySelectorAll(".boxH2");
+
+const boxP = document.querySelectorAll(".box p");
+
+boxH2.forEach((box, index) => {
+  box.addEventListener("click", function (e) {
+    boxP.forEach((p, i) => {
+      if (index == i) {
+        p.classList.toggle("display");
+      }
+    });
+  });
+});
