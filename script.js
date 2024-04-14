f919781622efa1fcf92faabb0ff8d21c35b04dc6
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2024-04-14 06:16:24 -0300

    scroll smooth

--- script.js
+++ script.js
@@ -1,56 +1,85 @@
-let loader = document.querySelector("#pre-loader");
 
-window.addEventListener("load", function () {
-  loader.style.display = "none";
-});
+function initLoader(){
+  let loader = document.querySelector("#pre-loader");
 
-const navbar = document.querySelector(".navbar");
-
-const navMenu = document.querySelector(".nav-menu");
-
-const hamburger = document.querySelector(".navbar");
-navbar.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-
-const articleBtn = document.querySelectorAll(".articleBtn");
-const atero = document.querySelector("#atero");
-const filosofia = document.querySelector("#filosofia");
-const risco = document.querySelector("#risco");
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-
-const artigo = document.querySelectorAll(".articleText");
-const cardArtigo = document.querySelectorAll(".article");
-const exit = document.querySelectorAll(".exit");
-
-function desaturateBg(){
-  header.style.filter = 'saturate(0)';
-  main.style.filter = 'saturate(0)';
+  window.addEventListener("load", function () {
+    loader.style.display = "none";
+  });
 }
+initLoader();
 
-function toExit(index){
-  artigo[index].style.display ='none';
-  header.style.filter = 'saturate(100%)';
-  main.style.filter = 'saturate(100%)';
+
+
+function initBurger(){
+  const navbar = document.querySelector(".navbar");
+  const navMenu = document.querySelector(".nav-menu");
+  const hamburger = document.querySelector(".navbar");
+  
+  navbar.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
 }
+initBurger();
 
-exit.forEach((eachExit, index) => {
-  eachExit.addEventListener("click", () => {
-    toExit(index);
+
+function initArticle(){
+  const articleBtn = document.querySelectorAll(".articleBtn");
+  const header = document.querySelector("header");
+  const main = document.querySelector("main");
+  
+  const artigo = document.querySelectorAll(".articleText");
+  const cardArtigo = document.querySelectorAll(".article");
+  const exit = document.querySelectorAll(".exit");
+  
+  function desaturateBg(){
+    header.style.filter = 'saturate(0)';
+    main.style.filter = 'saturate(0)';
+  }
+  
+  function toExit(index){
+    artigo[index].style.display ='none';
+    header.style.filter = 'saturate(100%)';
+    main.style.filter = 'saturate(100%)';
+  }
+  
+  exit.forEach((eachExit, index) => {
+    eachExit.addEventListener("click", () => {
+      toExit(index);
+    })
   })
-})
-
-function displayArticle(index){
-  artigo[index].style.display = 'block';
-  desaturateBg();
-}
-
-
-cardArtigo.forEach((card,index) => {
-  card.addEventListener('click', () => {
-    displayArticle(index)
+  
+  function displayArticle(index){
+    artigo[index].style.display = 'block';
+    artigo[index].classList.toggle("active");
+    desaturateBg();
+  }
+  
+  
+  cardArtigo.forEach((card,index) => {
+    card.addEventListener('click', () => {
+      displayArticle(index)
+    })
   })
-})
+}
+initArticle();
+
+
+function initScroll(){
+  const btnHref = document.querySelectorAll("a[href^='#']")
+  function toScroll(e){
+     e.preventDefault();
+     const href = e.currentTarget.getAttribute('href');
+     const section = document.querySelector(href);
+     section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+     })
+  }
+  
+  
+  btnHref.forEach((btn, e) => {
+    btn.addEventListener("click", toScroll)
+  })  
+}
+initScroll();
