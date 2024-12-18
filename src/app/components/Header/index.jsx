2fcc5dda12f66943bfe18a9d0e8b3912d58cb2e1
rafaelabou1999 <rafaelabourdette@gmail.com>
2024-12-18 20:27:24 -0300

    working on menu burger

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,18 +1,37 @@
 
+'use client'
+
 import styles from "./Header.module.css";
 import Icon from "@/../public/agendar.svg";
 import Image from "next/image";
 import Logo from "@/../public/renata.jpeg"
+import Burger from "@/../public/burger.svg"
+import { useState, useEffect } from 'react';
 export default function Header(){
+    const [size, setSize] = useState(window.innerWidth)
+    useEffect(() => {
+        const handleSize = () => setSize(window.innerWidth);
+        window.addEventListener('resize', handleSize)
+        return () => {
+            window.removeEventListener('resize', handleSize)
+        }
+    },[])
+    
     return (
         <header className={styles.container}>
-          <div className={styles.logo_container}>
+            <div className={styles.menu_burger}>
+            <div className={styles.logo_container}>
                 <Image src={Logo} className={styles.logo} alt="perfil Renata"/>
             <div className={styles.container_text}>
                 <h2 className={styles.logo_h2}>Dra. Renata Bourdette</h2>
                 <p className={styles.logo_p}>Cardiologista</p>
                 <p className={styles.logo_crm}>CRM: 604248-RJ</p>
             </div> 
+            </div>
+         
+            <div className={size  <= 1300 ? styles.burger : styles.isBurger}>
+                <Image src={Burger} alt="menu burger"/>
+            </div>
           </div>
           <div className={styles.line}></div>
             <ul className={styles.container_list}>
---
+++ src/app/components/MobileMenu/index.jsx
@@ -0,0 +1,11 @@
+import styles from './MobileMenu.module.css'
+
+export default function MobileMenu(){
+    return(
+        <div className={styles.container}>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
+        </div>
+    )
+}
