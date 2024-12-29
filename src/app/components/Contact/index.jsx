a529956c907af2c9b50a0b96e9a16c9bd2a78d76
rafaelabou1999 <rafaelabourdette@gmail.com>
2024-12-29 17:44:38 -0300

    working on responsive

--- src/app/components/Contact/index.jsx
+++ src/app/components/Contact/index.jsx
@@ -8,12 +8,18 @@ export default function Contact(){
     return(
         <div className={styles.container}>
             <div className={styles.each}>
-               <Image src={Phone} alt="icone" className={styles.icon}/>
+                <div className={styles.each_title}>
+                <Image src={Phone} alt="icone" className={styles.icon}/>
+                <h4 className={styles.each_h4}>Telefone</h4>
+                </div>
                <p className={styles.text}>+55 21 2323-2323</p> 
             </div>
 
             <div className={styles.each}>
+                <div className={styles.each_title}>
                 <Image src={Map} alt="icone" className={styles.icon}/>    
+                <h4 className={styles.each_h4}>Localização</h4>
+                </div>
                 <p className={styles.text}>Av. das Américas, 3.500 | Barra da Tijuca <br/> Ed. Toronto, Bloco 4, sala 606</p>   
             </div>
         </div>
