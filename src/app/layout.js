253d28b21b4adefdf6b49b2f078409c8a2eed28d
rafaelabou1999 <rafaelabourdette@gmail.com>
2025-01-07 12:35:41 -0300

    fixing content

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,8 @@ import Header from "./components/Header";
 export const metadata = {
   title: "Dra. Renata Bourdette",
   description: "Renata Bourdette é uma médica cardiologista formada pela UERJ e titulada como Especialista pela Sociedade Brasileira de Cardiologia.",
-  icon: "@/../public/rb.png"
+  icon: "@/../public/rb.png",
+  contentType: "application/javascript"
 };
 
 export default function RootLayout({ children }) {
