08d1bae9e0f2812230306795537c923d55245708
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2025-01-08 17:32:18 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
-  assetPrefix: process.env.URL ? process.env.URL : undefined,
-};
-
-export default nextConfig;
